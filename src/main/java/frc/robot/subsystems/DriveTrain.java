22b8ead8dd134bc1300d946d9ba09c6d26a4f719
Jordan Bancino <jordan@bancino.net>
2019-10-22 10:49:27 -0400

    Update for SwerveIO

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -3,8 +3,6 @@ package frc.robot.subsystems;
 import frc.robot.RobotMap;
 import frc.robot.commands.joystick.DriveWithJoystick;
 import net.bancino.robotics.swerveio.SwerveDrive;
-import net.bancino.robotics.swerveio.SwerveModule;
-import net.bancino.robotics.swerveio.module.AbstractSwerveModule;
 import net.bancino.robotics.swerveio.module.MK2SwerveModule;
 import net.bancino.robotics.swerveio.module.MultiEncoderModule;
 import net.bancino.robotics.swerveio.module.MultiEncoderModule.EncoderSetting;
@@ -16,35 +14,33 @@ import net.bancino.robotics.swerveio.module.MultiEncoderModule.EncoderSetting;
 public class DriveTrain extends SwerveDrive {
   public static final double BASE_WIDTH = 20;
   public static final double BASE_LENGTH = 22;
+  public static final double COUNTS_PER_PIVOT_REVOLUTION = 17.90471839904785;
 
   /**
    * Create the SwerveDrive with the default settings and the
    * robot map.
    */
   public DriveTrain() {
-    super(BASE_WIDTH, BASE_LENGTH,
+    super(BASE_WIDTH, BASE_LENGTH, COUNTS_PER_PIVOT_REVOLUTION,
       new MK2SwerveModule(RobotMap.FRONT_LEFT_DRIVE_MOTOR, RobotMap.FRONT_LEFT_PIVOT_MOTOR, RobotMap.FRONT_LEFT_ANALOG_ENCODER),
       new MK2SwerveModule(RobotMap.FRONT_RIGHT_DRIVE_MOTOR, RobotMap.FRONT_RIGHT_PIVOT_MOTOR, RobotMap.FRONT_RIGHT_ANALOG_ENCODER),
       new MK2SwerveModule(RobotMap.REAR_LEFT_DRIVE_MOTOR, RobotMap.REAR_LEFT_PIVOT_MOTOR, RobotMap.REAR_LEFT_ANALOG_ENCODER),
-      new MK2SwerveModule(RobotMap.REAR_RIGHT_DRIVE_MOTOR, RobotMap.REAR_RIGHT_PIVOT_MOTOR, RobotMap.REAR_RIGHT_ANALOG_ENCODER)
-    );
-    countsPerPivotRevolution = 17.90471839904785;
-    /**
-     * Here we set the module's settings. Set them for each module.
-     */
-    for (var modKey : moduleMap.keySet()) {
-      MultiEncoderModule module = (MultiEncoderModule) moduleMap.get(modKey);
-      /* TODO: Use the analog encoder! */
-      module.setEncoder(EncoderSetting.INTERNAL);
-      module.zeroDriveEncoder();
+      new MK2SwerveModule(RobotMap.REAR_RIGHT_DRIVE_MOTOR, RobotMap.REAR_RIGHT_PIVOT_MOTOR, RobotMap.REAR_RIGHT_ANALOG_ENCODER),
+      (module) -> {
+        MultiEncoderModule memodule = (MultiEncoderModule) module;
+        /* TODO: Use the analog encoder! */
+        memodule.setEncoder(EncoderSetting.INTERNAL);
+        memodule.zeroDriveEncoder();
 
-      module.setPivotClosedLoopRampRate(0);
-      module.setPivotPidP(0.1);
-      module.setPivotPidI(1e-4);
-      module.setPivotPidD(1);
-      module.setPivotPidIZone(0);
-      module.setPivotPidFF(0);
-    }
+        module.setPivotClosedLoopRampRate(0);
+        module.setPivotPidP(0.1);
+        module.setPivotPidI(1e-4);
+        module.setPivotPidD(1);
+        module.setPivotPidIZone(0);
+        module.setPivotPidFF(0);
+      }
+    );
+
   }
 
   @Override
