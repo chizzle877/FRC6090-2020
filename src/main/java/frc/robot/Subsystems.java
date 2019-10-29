9d00674e4530d4881ff329c735e75687f792d8ec
Jordan Bancino <jordan@bancino.net>
2019-10-29 18:45:58 -0400

    Use new SwerveIO changes

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -3,14 +3,25 @@ package frc.robot;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Gyro;
 
+import net.bancino.robotics.swerveio.exception.SwerveException;
+
 /**
- * Instantiate all the subsystems here for static reference,
- * since this is how WPI models it. These are declared final
- * to prevent subsystems from being arbitrarily modified by
- * other code.
+ * Instantiate all the subsystems here for static reference, since this is how
+ * WPI models it. These are declared final when possible to prevent subsystems
+ * from being arbitrarily modified by other code.
+ * 
  * @author Jordan Bancino
  */
 public class Subsystems {
-    public static final DriveTrain driveTrain = new DriveTrain();
+    public static DriveTrain driveTrain;
+
+    static {
+        try {
+            driveTrain = new DriveTrain();
+        } catch (SwerveException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static final Gyro gyro = new Gyro();
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,14 +1,20 @@
 package frc.robot.subsystems;
 
+import java.util.LinkedHashMap;
+
 import frc.robot.RobotMap;
 import frc.robot.commands.joystick.DriveWithJoystick;
 import net.bancino.robotics.swerveio.SwerveDrive;
+import net.bancino.robotics.swerveio.SwerveModule;
+import net.bancino.robotics.swerveio.module.AbstractSwerveModule;
 import net.bancino.robotics.swerveio.module.MK2SwerveModule;
 import net.bancino.robotics.swerveio.module.MultiEncoderModule;
 import net.bancino.robotics.swerveio.module.MultiEncoderModule.EncoderSetting;
+import net.bancino.robotics.swerveio.exception.SwerveException;
 
 /**
  * Our implementation of the Swerve Drive subsystem.
+ * 
  * @author Jordan Bancino
  */
 public class DriveTrain extends SwerveDrive {
@@ -17,29 +23,35 @@ public class DriveTrain extends SwerveDrive {
   public static final double COUNTS_PER_PIVOT_REVOLUTION = 17.90471839904785;
 
   /**
-   * Create the SwerveDrive with the default settings and the
-   * robot map.
+   * Create the SwerveDrive with the default settings and the robot map.
    */
-  public DriveTrain() {
+  public DriveTrain() throws SwerveException {
     super(BASE_WIDTH, BASE_LENGTH, COUNTS_PER_PIVOT_REVOLUTION,
-      new MK2SwerveModule(RobotMap.FRONT_LEFT_DRIVE_MOTOR, RobotMap.FRONT_LEFT_PIVOT_MOTOR, RobotMap.FRONT_LEFT_ANALOG_ENCODER),
-      new MK2SwerveModule(RobotMap.FRONT_RIGHT_DRIVE_MOTOR, RobotMap.FRONT_RIGHT_PIVOT_MOTOR, RobotMap.FRONT_RIGHT_ANALOG_ENCODER),
-      new MK2SwerveModule(RobotMap.REAR_LEFT_DRIVE_MOTOR, RobotMap.REAR_LEFT_PIVOT_MOTOR, RobotMap.REAR_LEFT_ANALOG_ENCODER),
-      new MK2SwerveModule(RobotMap.REAR_RIGHT_DRIVE_MOTOR, RobotMap.REAR_RIGHT_PIVOT_MOTOR, RobotMap.REAR_RIGHT_ANALOG_ENCODER),
-      (module) -> {
-        MultiEncoderModule memodule = (MultiEncoderModule) module;
-        /* TODO: Use the analog encoder! */
-        memodule.setEncoder(EncoderSetting.INTERNAL);
-        memodule.zeroDriveEncoder();
+        () -> {
+          var m = new LinkedHashMap<SwerveModule, AbstractSwerveModule>();
+          m.put(SwerveModule.FRONT_RIGHT, new MK2SwerveModule(RobotMap.FRONT_RIGHT_DRIVE_MOTOR,
+              RobotMap.FRONT_RIGHT_PIVOT_MOTOR, RobotMap.FRONT_RIGHT_ANALOG_ENCODER));
+          m.put(SwerveModule.FRONT_LEFT, new MK2SwerveModule(RobotMap.FRONT_LEFT_DRIVE_MOTOR,
+              RobotMap.FRONT_LEFT_PIVOT_MOTOR, RobotMap.FRONT_LEFT_ANALOG_ENCODER));
+          m.put(SwerveModule.REAR_LEFT, new MK2SwerveModule(RobotMap.REAR_LEFT_DRIVE_MOTOR,
+              RobotMap.REAR_LEFT_PIVOT_MOTOR, RobotMap.REAR_LEFT_ANALOG_ENCODER));
+          m.put(SwerveModule.REAR_RIGHT, new MK2SwerveModule(RobotMap.REAR_RIGHT_DRIVE_MOTOR,
+              RobotMap.REAR_RIGHT_PIVOT_MOTOR, RobotMap.REAR_RIGHT_ANALOG_ENCODER));
 
-        module.setPivotClosedLoopRampRate(0);
-        module.setPivotPidP(0.1);
-        module.setPivotPidI(1e-4);
-        module.setPivotPidD(1);
-        module.setPivotPidIZone(0);
-        module.setPivotPidFF(0);
-      }
-    );
+          return m; /* Return the module map for the constructor's use. */
+        }, (module) -> {
+          MultiEncoderModule memodule = (MultiEncoderModule) module;
+          /* TODO: Use the analog encoder! */
+          memodule.setEncoder(EncoderSetting.INTERNAL);
+          memodule.zeroDriveEncoder();
+
+          module.setPivotClosedLoopRampRate(0);
+          module.setPivotPidP(0.1);
+          module.setPivotPidI(1e-4);
+          module.setPivotPidD(1);
+          module.setPivotPidIZone(0);
+          module.setPivotPidFF(0);
+        });
 
   }
 
