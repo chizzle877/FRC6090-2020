3a3618a6de3b556f2b4d9e8ebd37518cd901c43c
Jordan Bancino <jordan@bancino.net>
2019-11-08 10:46:53 -0500

    Change the way subsystems are created

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -1,9 +1,27 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Gyro;
 
 import net.bancino.robotics.swerveio.exception.SwerveException;
+import net.bancino.robotics.swerveio.exception.SwerveRuntimeException;
+
+/**
+ * An interface that can be used to create subystems.
+ */
+@FunctionalInterface
+interface SubsystemCreator {
+
+    /**
+     * When a subsystem's constructor throws an exception, attempt to create the
+     * object and return it. If it fails, convert the exception into a runtime
+     * exception.
+     * @return A subsystem that can be cast to the specific type.
+     * @throws RuntimeException if an error creating the subsystem occurs.
+     */
+    public Subsystem create() throws RuntimeException;
+}
 
 /**
  * Instantiate all the subsystems here for static reference, since this is how
@@ -13,15 +31,18 @@ import net.bancino.robotics.swerveio.exception.SwerveException;
  * @author Jordan Bancino
  */
 public class Subsystems {
-    public static DriveTrain driveTrain;
-
-    static {
-        try {
-            driveTrain = new DriveTrain();
-        } catch (SwerveException e) {
-            e.printStackTrace();
+    /** Our drivetrain, which is a SwerveIO swerve drive. */
+    public static final DriveTrain driveTrain = (DriveTrain) new SubsystemCreator (){
+        @Override
+        public Subsystem create() throws RuntimeException {
+            try {
+                return new DriveTrain();
+            } catch (SwerveException e) {
+                throw new SwerveRuntimeException(e);
+            }
         }
-    }
+    }.create();
 
+    /** The gyro. */
     public static final Gyro gyro = new Gyro();
 }
